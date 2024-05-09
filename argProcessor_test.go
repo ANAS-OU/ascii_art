c613eb592dfb9ac588f3f2bb7f352b268eac5ebd
oanass <outiskteanas@gmail.com>
2024-05-09 14:25:32 +0100

    Unit-Testing files for argProcessor function

---
+++ argProcessor_test.go
@@ -0,0 +1,128 @@
+package ascii_art
+
+import (
+	"log"
+	"os"
+	"strings"
+	"testing"
+)
+
+const filename = "txt/expected.txt"
+const templatePath = "txt/standard.txt"
+const artCharHeight = 8
+
+type TestCase struct {
+	input    string
+	expected string
+	gotten   string
+}
+
+func getExpectedOutput(test_ID int) string {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Error! problem in reading %s.", filename)
+	}
+	contentList := strings.Split(string(content), "\n")
+	begin := test_ID * (artCharHeight + 1)
+	end := begin + artCharHeight
+	return strings.Join(contentList[begin:end], "\n")
+}
+
+func TestLowercase(t *testing.T) {
+	test := TestCase{
+		input:    "hello",
+		expected: getExpectedOutput(0),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
+
+func TestUppercase(t *testing.T) {
+	test := TestCase{
+		input:    "HELLO",
+		expected: getExpectedOutput(1),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
+
+func TestMixcase(t *testing.T) {
+	test := TestCase{
+		input:    "HeLlo HuMaN",
+		expected: getExpectedOutput(2),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
+
+func TestDigits(t *testing.T) {
+	test := TestCase{
+		input:    "1Hello 2There",
+		expected: getExpectedOutput(3),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
+
+func TestSymbols_A(t *testing.T) {
+	test := TestCase{
+		input:    "{Hello & There #}",
+		expected: getExpectedOutput(4),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
+
+func TestSymbols_B(t *testing.T) {
+	test := TestCase{
+		input:    "1a\"#FdwHywR&/()=",
+		expected: getExpectedOutput(5),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
+
+func TestSymbols_C(t *testing.T) {
+	test := TestCase{
+		input:    "\\!\" #$%&'()*+,-./",
+		expected: getExpectedOutput(6),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
+
+func TestSymbols_D(t *testing.T) {
+	test := TestCase{
+		input:    ":;<=>?@",
+		expected: getExpectedOutput(7),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
+
+func TestAlphabetic(t *testing.T) {
+	test := TestCase{
+		input:    "abcdefghijklmnopqrstuvwxyz",
+		expected: getExpectedOutput(8),
+	}
+	test.gotten = ArgProcessor(test.input, templatePath)
+	if test.expected != test.gotten {
+		t.Fail()
+	}
+}
