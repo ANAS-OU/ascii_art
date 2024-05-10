27cd3d6d5ba4fed9ed4b163c80e1f1e546663596
anas-ou <anas.outiskte@edu.uiz.ac.ma>
2024-05-10 18:05:22 +0100

    fix

--- argProcessor.go
+++ argProcessor.go
@@ -15,8 +15,8 @@ import (
  * Return : string
  */
 func Gathering(input, content string, line int) string {
-	// Spliting the content of the standard
-	// file in a slice of strings
+	// Spliting the content of the standard file
+	// in a slice of strings
 	standard := strings.Split(content, "\n")[1:]
 
 	var slc []string
